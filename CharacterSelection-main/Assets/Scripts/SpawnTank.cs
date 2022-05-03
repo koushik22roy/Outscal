2f7ca6e8fcedb241a7d1b1acc15b6156a4693de7
Koushik Roy <koushik1858roy@gmail.com>
2022-05-03 12:56:44 +0530

    Tank Prefab , and Spawning tank

---
+++ CharacterSelection-main/Assets/Scripts/SpawnTank.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTank : MonoBehaviour
+{
+    [SerializeField] GameObject tankprefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawning
+        Instantiate(tankprefab, transform.position, Quaternion.identity); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
